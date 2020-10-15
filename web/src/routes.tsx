fb0552fe90405c0b282e62e71ee8a2500104ccfc
joaovitors1g <joao_vitor9846@hotmail.com>
2020-10-15 20:52:34 -0300

    feat(web): add new routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
 const Routes: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/orphanages" component={OrphanagesMap} />
+        <Route path="/orphanages" exact component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </Router>
   )
