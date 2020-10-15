19b9783949b497f5a0b9c1c9416981af84c12a87
joaovitors1g <joao_vitor9846@hotmail.com>
2020-10-15 20:47:48 -0300

    feat: add server main file

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+import './database/connection';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('App listening at port 3333 🚀');
+});
