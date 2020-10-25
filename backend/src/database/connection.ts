844650909a1dd95eec3acf6341f0af9fa809f880
joaovitors1g <joao_vitor9846@hotmail.com>
2020-10-25 15:45:29 -0300

    feat: use env variables for db connection

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,3 +1,12 @@
 import { createConnection } from 'typeorm';
+import ormConfig from '../../ormconfig.json';
+const { DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
 
-createConnection();
+createConnection({
+  ...ormConfig,
+  name: DB_NAME as string,
+  host: DB_HOST as string,
+  port: DB_PORT as string,
+  password: DB_PASSWORD as string,
+  username: DB_USER as string,
+});
