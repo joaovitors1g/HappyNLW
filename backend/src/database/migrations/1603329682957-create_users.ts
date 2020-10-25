61a9b538c48ddf59487268a8c81e54961d409155
joaovitors1g <joao_vitor9846@hotmail.com>
2020-10-25 15:45:39 -0300

    feat: add create_user migration

---
+++ backend/src/database/migrations/1603329682957-create_users.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsers1603329682957 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password_hash',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar_url',
+            type: 'varchar',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
