464042c370c2db50d2c81d685db77c6550fe7f80
joaovitors1g <joao_vitor9846@hotmail.com>
2020-10-26 22:01:21 -0300

    fix: typeorm not finding default connection

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -4,9 +4,9 @@ const { DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
 
 createConnection({
   ...ormConfig,
-  name: DB_NAME as string,
+  database: DB_NAME as string,
   host: DB_HOST as string,
   port: DB_PORT as string,
   password: DB_PASSWORD as string,
   username: DB_USER as string,
-});
+} as any);
